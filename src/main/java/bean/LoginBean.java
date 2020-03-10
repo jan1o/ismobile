5f131e1e7f7f77d19eebb6221c3eea2e8cacef0c
Felipe Serafim <felipe.gabriel.mariz@hotmail.com>
2020-03-10 10:55:24 -0300

    Login pronto com erro no JPAUtil

---
+++ src/main/java/bean/LoginBean.java
@@ -0,0 +1,62 @@
+package bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.persistence.EntityManager;
+
+import model.Usuario;
+import repository.Usuarios;
+import util.JpaUtil;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+	
+	private Usuario usuario = null;
+	
+	private String nome;
+	private String senha;
+
+	public void envia() {
+		EntityManager manager = JpaUtil.getEntityManager();
+		Usuarios usuarioDAO = new Usuarios(manager);
+		usuario = usuarioDAO.usuarioPorNome(this.nome);
+		if (usuario == null) {
+			usuario = new Usuario();
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!", "Erro no Login!"));
+		
+		} else if(usuario != null && usuario.getSenha_usuario().equals(this.senha)) {
+			System.out.println("Logado");
+		}
+		
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
