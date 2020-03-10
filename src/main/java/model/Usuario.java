f42e75cba31105fa56427abc86d3baffe57dc901
Janio Fernandes <janiojr14@hotmail.com>
2020-03-10 07:51:00 -0300

    config inicial

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+}
